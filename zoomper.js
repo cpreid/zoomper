f72c4fcd2f3c0d5f572afc2664fa1083ba86016d
Cooper Reid <cooper@Coopers-MBP-2.home>
2016-01-03 14:05:53 -0500

    loading state

--- zoomper.js
+++ zoomper.js
@@ -17,7 +17,7 @@ var Zoomper = function(elt, small, large) {
     this.frameSize = {'width': parseInt(elt.style.width), 'height': parseInt(elt.style.height)};
     this.mode = 'out';
     this.cursorStates = {'in': 'zoom-out', 'out': 'zoom-in'};
-    this.initComplete = false;
+    this.largeLoaded = false;
 }
 Zoomper.prototype = {
     positionZoom: function(evt) {
@@ -52,9 +52,8 @@ Zoomper.prototype = {
         this.mode = this.mode === 'in' ? 'out' : 'in';
         this.zoomState(this.mode);              
     },
-    loadImagesCB: function(cb) {
-        var toLoad = [this.smallImg, this.largeImg],
-            loaded = 0,
+    loadImagesCB: function(toLoad, cb) {
+        var loaded = 0,
             checkComplete = function() {
                 loaded++;
                 if(toLoad.length === loaded) cb();
@@ -65,14 +64,37 @@ Zoomper.prototype = {
             img.src = toLoad[i];
         }
     },
+    loadState: function(loading) {
+        if(loading) this.loadElt.style.display = 'block';
+        else this.loadElt.style.display = 'none';
+    },
+    clickHandler: function(evt) {
+        var _this = this;
+        // show zoom immediately
+        if(_this.largeLoaded) {
+            _this.zoomStateToggle(); 
+            _this.positionZoom(evt);
+        }
+        // show loading state while zoom image loads
+        else {
+            _this.loadState(true);
+            _this.loadImagesCB([_this.largeImg], function() {
+                _this.largeLoaded = true;
+                _this.clickHandler(evt);
+                _this.loadState(false);
+            });
+        }        
+    },
     init: function() {
         var _this = this;
         _this.elt.style.backgroundRepeat = 'no-repeat';
+        _this.elt.style.position = 'relative';
         _this.zoomState('out');
-        _this.elt.addEventListener('click', function(evt) { 
-            _this.zoomStateToggle(); 
-            _this.positionZoom(evt);
-        });           
+        _this.elt.addEventListener('click', function(evt) { _this.clickHandler(evt); });
+        _this.loadElt = document.createElement('div');
+        _this.loadElt.style.cssText = 'display:none;height:100%;width:100%;top:0;left:0;position:absolute;background:rgba(255,255,255,.6);';
+        _this.loadElt.innerHTML = '<div style="height:100%;width:100%;top:0;left:0;position:absolute;background: url(\'data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==\') center center no-repeat;"></div>';
+        _this.elt.appendChild(_this.loadElt);
         return _this;
     }
 }
